7d810b380ac7682c8ab8612514f85551af04e28e
Dr. Ada <contact@maidcoin.org>
2021-08-18 22:53:21 +0900

    mainnet test

--- scripts/deployScript.ts
+++ scripts/deployScript.ts
@@ -3,9 +3,10 @@ import hardhat from "hardhat";
 async function main() {
     console.log("deploy start")
 
-    const TestLPToken = await hardhat.ethers.getContractFactory("TestLPToken")
-    const testLPToken = await TestLPToken.deploy()
-    console.log(`TestLPToken address: ${testLPToken.address}`)
+    const SushiGirl = await hardhat.ethers.getContractFactory("SushiGirl")
+    //TODO: THIS IS FOR TEST
+    const sushiGirl = await SushiGirl.deploy("0x397FF1542f962076d0BFE58eA045FfA2d347ACa0", "0x6B3595068778DD592e39A122f4f5a5cF09C90fE2")
+    console.log(`SushiGirl address: ${sushiGirl.address}`)
 }
 
 main()
